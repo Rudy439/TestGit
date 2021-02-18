3f4fda0cae9d56ba2bb13f6a9a386834e97f3c9b
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-18 21:28:39 +0100

    Update Main.java
    changed

--- Main.java
+++ Main.java
@@ -4,12 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Queue<Integer> fronta = new Queue<>(c:4);
         try {
+            // fronta
             fronta.enqueue(value:24);
             fronta.enqueue(value:31);
+            fronta.enqueue(value:8888);
 
+            fronta.print();
             fronta.print();
             fronta.dequeue();
             fronta.print();
         }
+        public int hello(){
+            return 5;
+        }
+        //koniec
     }
 }
