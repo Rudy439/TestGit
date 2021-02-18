d4ddca30eca4c31e845a59ac03770adb49bd4abd
Rudy439 <rudolf.mato@kosickaakademia.sk>
2021-02-18 21:06:30 +0100

    zmena

--- Main.java
+++ Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Queue<Integer> fronta = new Queue<>(c:4);
         try {
-            fronta.enqueue(value:15);
-            fronta.enqueue(value:26);
+            fronta.enqueue(value:24);
             fronta.enqueue(value:31);
 
             fronta.print();
